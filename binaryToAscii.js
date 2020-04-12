447b68bf76837a0c95e540346c23edd32f65146d
Valter Cvijic <valter.cvijic@dlabs.si>
2020-04-12 03:39:22 +0200

    Simplify binaryToAscii solution

--- binaryToAscii.js
+++ binaryToAscii.js
@@ -2,13 +2,7 @@
 // https://www.codewars.com/kata/5583d268479559400d000064/javascript
 
 const binaryToString = (binary) => {
-  let result = "";
-  const arr = binary.match(/.{1,8}/g);
-
-  for (let i = 0; i < arr.length; i++) {
-    result += String.fromCharCode(parseInt(arr[i], 2).toString(10));
-  }
-  return result;
+  return binary.replace(/[01]{8}/g, (x) => String.fromCharCode(parseInt(x, 2)));
 };
 
 binaryToString("01100001"); // "a"
